e5eb5bd4c0d072419abafc239eca0380ba64ef6c
Lucas Jose da Silva <07055338166@tocantins.governo>
2024-05-22 18:01:22 -0300

    Criando os Models, Serializers e Views

---
+++ django_study/models.py
@@ -0,0 +1,11 @@
+from django.db  import models 
+
+
+class Pessoa (models.MOdel):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=14)
+    
+    
+    def __str__(self):
+        return self.name
+    
